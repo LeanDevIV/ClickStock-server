e361c8ab9c630966bda436011243bbc9f59bcd4a
LeanDevIV <leandrocordoba318@gmail.com>
2025-10-30 18:14:04 -0300

    feat: conexión inicial con la base de datos y estructura de rutas
    - Se agregó el archivo de configuración de la base de datos (`src/config/db.js`)
    - Se creó el archivo de rutas principales (`src/routes/indexRoutes.js`)
    - Se actualizaron las dependencias en `package.json` y `package-lock.json`
    - Se modificó `index.js` para integrar la conexión con la base de datos y las rutas principales

---
+++ src/config/db.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const MONGO_URI = process.env.MONGO_URI;
+
+
+export const conexionBD = async () => {
+    try {
+      await mongoose.connect(MONGO_URI);
+      console.log("✅ Conexión con base de datos exitosa");
+    } catch (error) {
+      console.error("❌ Fallo al conectar con base de datos:", error);
+      process.exit(1);
+    }
+  
+    mongoose.connection.on("Desconexión", () => {
+      console.warn("⚠️ Base de datos desconectada");
+    });
+  
+    mongoose.connection.on("Reconexión", () => {
+      console.log("🔄 Base de datos reconectada");
+    });
+  };
