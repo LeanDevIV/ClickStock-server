de782bb14c734f625c707d2d08f2453256f70c8e
sara-18git <saraabigailrobles9@gmail.com>
2025-11-01 20:04:05 -0300

    feat:validacion de token + rutas protegidas

---
+++ src/middleware/validacionDeToken.js
@@ -0,0 +1,15 @@
+import jwt from "jsonwebtoken";
+
+export const ValidacionDeToken = (req, res, next) => {
+  try {
+    const token = req.header("Authorization")?.replace("Bearer ", "");
+    if (!token) {
+      return res.status(401).json({ message: "Token requerido" });
+    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.usuario = decoded;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Token inválido o expirado" });
+  }
+};
