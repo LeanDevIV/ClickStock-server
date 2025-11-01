0ef49c18145bc3c39625f1d45bf226fcfe748c3d
sara-18git <saraabigailrobles9@gmail.com>
2025-11-01 18:06:45 -0300

    feat: implemente autenticación JWT con login y registro

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 import express from "express";
 import cors from "cors";
 import routes from "./src/routes/indexRoutes.js";
+import userRoutes from "./src/routes/usuarioRoutes.js";
 import { conexionBD } from "./src/config/db.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-await conexionBD();
+const startServer = async () => {
+  try {
+    await conexionBD(); 
 
-app.use(cors());
-app.use(express.json());
+    app.use(cors());
+    app.use(express.json());
 
-app.use("/api", routes);
 
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+    app.use("/usuarios", userRoutes);
+    app.use("/api", routes);
 
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on port ${PORT}`);
+    });
+
+  } catch (error) {
+    console.error("❌ Error al iniciar el servidor:", error);
+  }
+};
+
+startServer();
