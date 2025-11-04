fbce796768b3ae5ccfd6b8deb06ee0d74d2da0ec
agustin <alv2162@gmail.com>
2025-11-03 21:29:13 -0300

    se crean rutas controladores y servicios de reseñas

---
+++ src/models/Review.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const reviewSchema = new mongoose.Schema({
+  productId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+  user: { type: String, required: true },
+  rating: { type: Number, min: 1, max: 5, required: true },
+  comment: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+export default mongoose.model("Review", reviewSchema);
