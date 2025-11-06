3f0d1301f529478517f172b5ae5c8a31b7b568a6
sara-18git <saraabigailrobles9@gmail.com>
2025-11-06 20:43:15 -0300

    ruteo de la entidad pedidos

--- src/routes/indexRoutes.js
+++ src/routes/indexRoutes.js
@@ -4,6 +4,7 @@ import usuarioRoutes from "./usuarioRoutes.js";
 import productosRoutes from "./productosRoutes.js";
 import favoritosRoutes from "./favoritosRoutes.js";
 import carritoRoutes from "./carritoRoutes.js";
+import pedidosRoutes from "./pedidos.routes.js"
 
 
 const routes = Router();
@@ -12,6 +13,6 @@ routes.use("/usuarios", usuarioRoutes);
 routes.use("/productos", productosRoutes);
 routes.use("/favoritos", favoritosRoutes);
 routes.use("/carrito", carritoRoutes);
-
+routes.use("/pedidos",pedidosRoutes)
 
 export default routes;
