34de428737b95cb20d87bf814044985ab5616a4b
sara-18git <saraabigailrobles9@gmail.com>
2025-11-21 23:48:52 -0300

    reafctor:cambie los nombres a español y puse lo q se esperaba de los modelos

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -1,14 +1,14 @@
 import * as paymentService from "../services/payment.service.js";
 
-export const createPreference = async (req, res) => {
+export const crearPreferencia = async (req, res) => {
   try {
-    const { items, user } = req.body;
-    const returnUrl = req.headers.origin || "http://localhost:5173";
-    const pref = await paymentService.createPreference(items, returnUrl, user);
+    const { productos, usuario } = req.body;
+    const urlRetorno = req.headers.origin || "http://localhost:5173";
+    const preferencia = await paymentService.crearPreferencia(productos, urlRetorno, usuario);
 
-    return res.json({ id: pref.id, init_point: pref.init_point });
-  } catch (err) {
-    console.error("Error creando preferencia:", err);
+    return res.json({ id: preferencia.id, init_point: preferencia.init_point });
+  } catch (error) {
+    console.error("Error creando preferencia:", error);
     return res.status(500).json({ error: "Error creando preferencia" });
   }
 };
