8fb81b93bdb98dea87fa19b813e8fa96670f5e2d
sara-18git <saraabigailrobles9@gmail.com>
2025-11-22 01:14:05 -0300

    feat:agregue validaciones basicas en el ruteo de payment

--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -1,7 +1,26 @@
 import { Router } from "express";
+import { body } from "express-validator";
 import { crearPreferencia } from "../controllers/payment.controller.js";
 
 const router = Router();
-router.post("/crear_preferencia", crearPreferencia);
+const validarCrearPreferencia = [
+  body("productos")
+    .isArray({ min: 1 })
+    .withMessage("Debe haber al menos un producto"),
+  
+  body("productos.*.precio")
+    .isNumeric()
+    .withMessage("El precio debe ser un número"),
+  
+  body("productos.*.cantidad")
+    .isInt({ min: 1 })
+    .withMessage("La cantidad debe ser mayor a 0"),
+  
+  body("usuario.emailUsuario")
+    .isEmail()
+    .withMessage("Email debe ser válido")
+];
+
+router.post("/crear_preferencia", validarCrearPreferencia, crearPreferencia);
 
 export default router;
