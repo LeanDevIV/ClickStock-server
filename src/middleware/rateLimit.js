9eb05c5c50ac0dde1c35c6c43a902229d1ab98bb
LeanDevIV <leandrocordoba318@gmail.com>
2025-11-29 02:24:09 -0300

    Aumento de limite de consultas

--- src/middleware/rateLimit.js
+++ src/middleware/rateLimit.js
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 export const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 100,
+  max: 1000,
   message:
     "Demasiadas consultas desde esta direccion IP, por favor intente de nuevo en 15 minutos",
 });
