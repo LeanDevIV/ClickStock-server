5e885752872d9c49d01ff4bf3da40f4f61ed70ed
sara-18git <saraabigailrobles9@gmail.com>
2025-11-29 20:11:31 -0300

    fix:agregue la ruta de pedido para el usuario

--- src/routes/pedidos.routes.js
+++ src/routes/pedidos.routes.js
@@ -11,40 +11,27 @@ router.get(
 );
 
 router.post("/", ValidacionDeToken, pedidoController.crearPedido);
-
-// 2. OBTENER TODOS: Solo ADMIN puede ver TODOS los pedidos
 router.get(
   "/",
   ValidacionDeToken,
   validacionDeRol("admin"),
   pedidoController.obtenerPedidos
 );
-
-// 4. OBTENER UN PEDIDO: Solo requiere token
 router.get("/:id", ValidacionDeToken, pedidoController.obtenerPedido);
-
-// 5. ACTUALIZAR PEDIDO: Solo ADMIN o el propietario pueden actualizar (Implementación de seguridad en el controlador)
 router.put("/:id", ValidacionDeToken, pedidoController.actualizarPedido);
-
-// 6. ACTUALIZAR ESTADO: Solo ADMIN puede cambiar el estado del flujo
 router.patch(
   "/:id/estado",
   ValidacionDeToken,
   validacionDeRol("admin"),
   pedidoController.actualizarEstado
 );
-
-// 7. ELIMINAR (Soft Delete): Requiere token
 router.delete("/:id", ValidacionDeToken, pedidoController.eliminarPedido);
-
-// Borrado permanente (solo admin) - Ya estaba bien
 router.delete(
   "/permanent/:id",
   ValidacionDeToken,
   validacionDeRol("admin"),
   pedidoController.eliminarPedidoPermanente
 );
-// Restaurar pedido (solo admin) - Ya estaba bien
 router.patch(
   "/restore/:id",
   ValidacionDeToken,
