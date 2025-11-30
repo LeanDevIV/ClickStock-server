d095e244f62fba7bcb3baf80e904f324dea30062
LeanDevIV <leandrocordoba318@gmail.com>
2025-11-30 00:51:25 -0300

    Hotfix para admitir desarrollo local y de deploy con cors

--- index.js
+++ index.js
@@ -26,8 +26,22 @@ const PORT = process.env.PORT || 5000;
 app.use(helmet());
 app.use("/api", limiter);
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://clickstock-beta.vercel.app",
+  process.env.FRONTEND_URL,
+].filter(Boolean);
+
 const corsOptions = {
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("No permitido por CORS"));
+    }
+  },
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
   optionsSuccessStatus: 204,
